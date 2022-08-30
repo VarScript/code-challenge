9bb76e97b1aef445557eccf85d58e0fcadcb3f58
DevHars <vargassanchezfabio01@gmail.com>
2022-08-30 13:50:50 -0500

    fizzbuzz otehr solution

--- src/challenge0.js
+++ src/challenge0.js
@@ -25,3 +25,12 @@ for (let i = 1; i <= 100; i++) {
     else if (buzz) console.log(`buzz\n`);
     else console.log(`${i}\n`);
 }
+
+// -- Other solution 
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) console.log(`fizzbuzz\n`);
+    else if(i % 3 === 0) console.log(`fizz\n`);
+    else if(i % 5 === 0) console.log(`buzz\n`);
+    else console.log(`${i}\n`);
+}
