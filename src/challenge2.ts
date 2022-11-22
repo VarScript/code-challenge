f4de51865babee0e35df0062f429531173df3e16
DevHars <vargassanchezfabio01@gmail.com>
2022-11-22 17:26:58 -0500

    changes

--- src/challenge2.ts
+++ src/challenge2.ts
@@ -17,6 +17,21 @@
     *
     */
 
+    const isPrimary = (number:number) => {
+        let random:number = Math.random();
+        random *= 100 + 1;
+        random = Math.trunc(random);
+        console.log(random);
+        
+        if (number % 2 + 1 === 0 && number % 1 === 0 ){
+            console.log(`The number ${number} is primary`);
+        } else {
+            console.log('The number is not primary');
+        }
+    }
 
+    isPrimary(179)
 
-})
+    //  
+
+})()
--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
 "use strict";
-for (let i = 1; i <= 100; i++) {
-    let fizz = i % 3 === 0;
-    let buzz = i % 5 === 0;
-    if (fizz && buzz)
-        console.log(`fizzbuzz\n`);
-    else if (fizz)
-        console.log(`fizz\n`);
-    else if (buzz)
-        console.log(`buzz\n`);
-    else
-        console.log(`${i}\n`);
-}
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0)
-        console.log(`fizzbuzz\n`);
-    else if (i % 3 === 0)
-        console.log(`fizz\n`);
-    else if (i % 5 === 0)
-        console.log(`buzz\n`);
-    else
-        console.log(`${i}\n`);
-}
+(() => {
+    const isPrimary = (number) => {
+        let random = Math.random();
+        random *= 100 + 1;
+        random = Math.trunc(random);
+        console.log(random);
+        if (number % 2 + 1 === 0 && number % 1 === 0) {
+            console.log(`The number ${number} is primary`);
+        }
+        else {
+            console.log('The number is not primary');
+        }
+    };
+    isPrimary(179);
+})();
 //# sourceMappingURL=main.js.map
